37d09dbeaf132b9aa07fac6b8620ede453570143
Gulsum Gudukbay <gudukbay@gmail.com>
2017-12-26 13:10:16 +0300

    fixing bugs...

--- src/angle.cu
+++ src/angle.cu
@@ -30,34 +30,37 @@ __device__ double atomicAdd2(double* address, double val)
 
 __device__ void dot_product(const double *A, const double *B, int numElements, int blockSize, int width_thread, double *result)
 {
-	int start = blockIdx.x * blockSize + threadIdx.x;
+	int start = width_thread*(blockIdx.x * blockSize + threadIdx.x);
 	for(int i = start; i < start+width_thread; i++)
-		result[blockIdx.x]+= A[start+i]*B[start+i];
-		//atomicAdd2(&result[blockIdx.x], A[start + i] * B[start+i]);
+	{
+		if(i < numElements)
+			atomicAdd2(&result[blockIdx.x], A[i] * B[i]);
+	    //__syncthreads();
+	}
+
 }
 
 __device__ void mag_squared(const double *A, int numElements, int blockSize, int width_thread, double *result)
 {
-	int start = blockIdx.x * blockSize + threadIdx.x;
+	int start =width_thread*(blockIdx.x * blockSize + threadIdx.x);
 
 	//sum all elements squared in the block
 	for(int i = start; i < start+width_thread; i++)
 	{
-		printf("A[%d]: %.2f\n", start+i, A[start+i]);
-		result[blockIdx.x] += pow(A[start+i], 2);
-		//atomicAdd2(&result[blockIdx.x], pow(A[start + i], 2));
+		if(i < numElements)
+			atomicAdd2(&result[blockIdx.x], pow(A[i], 2));
+	    //__syncthreads();
 	}
+
 }
 
 __global__ void find_angle(const double *A, const double *B, int numElements, int blockSize, int width_thread, double *mag1, double *mag2, double *dot_prod)
 {
-	printf("\nhello\n");
-	for(int i = 0; i < numElements; i++)
-		printf("A[]: %.2f\n", i);
-	mag_squared(A, numElements, blockSize, width_thread, mag1);
-	mag_squared(B, numElements, blockSize, width_thread, mag2);
-	dot_product(A, B, numElements, blockSize, width_thread, dot_prod);
-	__syncthreads();
+
+	mag_squared(A, numElements, blockSize, width_thread+1, mag1);
+	mag_squared(B, numElements, blockSize, width_thread+1, mag2);
+	dot_product(A, B, numElements, blockSize, width_thread+1, dot_prod);
+    __syncthreads();
 }
 
 
@@ -76,32 +79,110 @@ double findAngleCPU(const double *A, const double *B, int numElements)
 		mag2 += pow(B[i], 2);
 	}
 
-	res = acos(dot_prod/(sqrt(mag1)*sqrt(mag2)));
+	mag1 = sqrt(mag1);
+	mag2 = sqrt(mag2);
+	//printf("mag1: %.2f, mag2: %.2f, dot_prod: %.2f\n", (float)mag1, (float)mag2, (float)dot_prod);
+	res = acos(dot_prod/(mag1*mag2));
 	return res;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	int N, blockSize;
+	srand (58);
+
+	/*FILE* in = fopen("input.txt", "w+");
+
+	fprintf(in, "%f\n", (float)1000000);
+	for( int i = 0; i < 2000000; i++)
+	{
+		fprintf(in, "%f\n", (float)(rand() / (RAND_MAX / 100)));
+	}
+	fclose(in);
+*/
+
+	int N, blockSize, threadElts;
 	double *A, *B, *d_A, *d_B;
 	double *dot_prod, *mag1, *mag2;
 	double *h_dot_prod, *h_mag1, *h_mag2;
-	srand (time ( NULL));
 
-	N = 1000000;
-	blockSize = 512;
-	cudaDeviceProp properties;
-	cudaGetDeviceProperties(&properties, 0);
-	printf("using %d multiprocessors\n", properties.multiProcessorCount);
-	printf("max threads per processor: %d\n", properties.maxThreadsPerMultiProcessor);
+	char* filename;
+	dot_prod = NULL;
+	mag1 = NULL;
+	mag2 = NULL;
+	threadElts = 256;
+	N = atoi(argv[1]);
+	blockSize = atoi(argv[2]);
 
-	int width_thread = N / blockSize/512;
-	int no_of_blocks = (int)ceil( N / blockSize/512);
+	if(argc == 4)
+		filename = argv[3];
 
+
+	cudaEvent_t start4, stop4;
+	cudaEventCreate(&start4);
+	cudaEventCreate(&stop4);
+	cudaEventRecord(start4);
+
+	if(argc == 3)
+	{
+		A = (double*)malloc(N * sizeof(double));
+		B = (double*)malloc(N * sizeof(double));
+
+		//fill in the arrays with random numbers
+		for(int i = 0; i < N; i++)
+		{
+			A[i] = rand() / (RAND_MAX / 100);
+		}
+		for(int i = 0; i < N; i++)
+		{
+			B[i] = rand() / (RAND_MAX / 100);
+		}
+	}
+	else
+	{
+		FILE * file;
+		int i;
+		float tmp;
+
+		if ((file = fopen(filename, "r+")) == NULL)
+		{
+			printf("ERROR: file open failed\n");
+			return -1;
+		}
+		fscanf(file,"%f", &tmp);
+		N = (int)tmp;
+		printf("%f\n", tmp);
+		A = (double*)malloc(N * sizeof(double));
+		B = (double*)malloc(N * sizeof(double));
+
+		for(i = 0; i < N; i++)
+		{
+			fscanf(file,"%f", &tmp);
+			A[i] = tmp;
+		}
+		for(i = 0; i < N; i++)
+		{
+			fscanf(file,"%f", &tmp);
+			B[i] = tmp;
+		}
+		fclose(file);
+
+	}
+
+	cudaEventRecord(stop4);
+
+	cudaEventSynchronize(stop4);
+	float milliseconds4 = 0;
+	cudaEventElapsedTime(&milliseconds4, start4, stop4);
+	printf("Time for the array generation: %f ms\n", milliseconds4);
+
+	int no_of_blocks = (int)ceil( N / blockSize / threadElts)+1;
+
+	printf("\nInfo\n______________________________________________________\n");
+	printf("Number of elements: %d\n", N);
+	printf("Number of threads per block: %d\n", blockSize);
 	printf("Number of blocks will be created: %d\n", no_of_blocks);
+	printf("\nTime\n______________________________________________________\n");
 
-	A = (double*)malloc(N * sizeof(double));
-	B = (double*)malloc(N * sizeof(double));
 
 	h_dot_prod = (double*)malloc(no_of_blocks * sizeof(double));
 	h_mag1 = (double*)malloc(no_of_blocks * sizeof(double));
@@ -112,25 +193,25 @@ int main()
 	magnitude1 = 0.0;
 	magnitude2 = 0.0;
 
-	//fill in the arrays with random numbers
-	for(int i = 0; i < N; i++)
-	{
-		A[i] = rand() / (RAND_MAX / 1000);
-	}
-	for(int i = 0; i < N; i++)
-	{
-		B[i] = rand() / (RAND_MAX / 1000);
-	}
-
 	//Compute angle on CPU
-	printf("Angle on CPU: %.2f\n", (180.0 / M_PI)*findAngleCPU(A, B, N));
+	cudaEvent_t start3, stop3;
+	cudaEventCreate(&start3);
+	cudaEventCreate(&stop3);
+	cudaEventRecord(start3);
+	float cpu_result = (float)((180.0 / M_PI)*findAngleCPU(A, B, N));
+	cudaEventRecord(stop3);
 
+	cudaEventSynchronize(stop3);
+	float milliseconds3 = 0;
+	cudaEventElapsedTime(&milliseconds3, start3, stop3);
+	printf("Time for the CPU function: %f ms\n", milliseconds3);
 
 	cudaMalloc(&d_A, N * sizeof(double));
 	cudaMalloc(&d_B, N * sizeof(double));
 	cudaMalloc(&dot_prod, no_of_blocks*sizeof(double));
 	cudaMalloc(&mag1, no_of_blocks*sizeof(double));
 	cudaMalloc(&mag2, no_of_blocks*sizeof(double));
+
 
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
@@ -145,41 +226,61 @@ int main()
 	float milliseconds = 0;
 	cudaEventElapsedTime(&milliseconds, start, stop);
 	printf("Time for Host to Device transfer: %f ms\n", milliseconds);
-	find_angle<<<no_of_blocks, blockSize>>>(d_A, d_B, N, blockSize, width_thread, mag1, mag2, dot_prod);
+
+	//KERNEL
+	cudaEvent_t start2, stop2;
+	float milliseconds2 = 0;
+
+	cudaEventCreate(&start2);
+	cudaEventCreate(&stop2);
+	cudaEventRecord(start2);
+	find_angle<<<no_of_blocks, blockSize>>>(d_A, d_B, N, blockSize, threadElts, mag1, mag2, dot_prod);
+	cudaDeviceSynchronize();
+	cudaEventRecord(stop2);
+
+	cudaEventSynchronize(stop2);
+	cudaEventElapsedTime(&milliseconds2, start2, stop2);
+	printf("Time for the kernel execution: %f ms\n", milliseconds2);
+
 	cudaError_t error = cudaGetLastError();
-		if (error != cudaSuccess) {
-		  fprintf(stderr, "ERROR: %s \n", cudaGetErrorString(error));
-	}
-	cudaThreadSynchronize();
-
-	for(int i = 0; i < no_of_blocks; i++)
-	{
-		magnitude1 += mag1[i];
-		magnitude2 += mag2[i];
-		dot_product += dot_prod[i];
+	if (error != cudaSuccess) {
+	  fprintf(stderr, "ERROR: %s \n", cudaGetErrorString(error));
 	}
 
-	printf("magnitude1: %.2f, magnitude2: %.2f, dot_product: %.2f\n", magnitude1, magnitude2, dot_product);
+	cudaEvent_t start5, stop5;
+	float milliseconds5 = 0;
+
+	cudaEventCreate(&start5);
+	cudaEventCreate(&stop5);
+	cudaEventRecord(start5);
 
 	cudaMemcpy(h_dot_prod, dot_prod, no_of_blocks*sizeof(double), cudaMemcpyDeviceToHost);
 	cudaMemcpy(h_mag1, mag1, no_of_blocks*sizeof(double), cudaMemcpyDeviceToHost);
 	cudaMemcpy(h_mag2, mag2, no_of_blocks*sizeof(double), cudaMemcpyDeviceToHost);
 
+	cudaEventRecord(stop5);
 
+	cudaEventSynchronize(stop5);
+	cudaEventElapsedTime(&milliseconds5, start5, stop5);
+	printf("Time for the Device to Host transfer: %f ms\n", milliseconds5);
+	printf("Total execution time for GPU: %f ms\n", milliseconds5 + milliseconds2 + milliseconds);
 
 	for(int i = 0; i < no_of_blocks; i++)
 	{
-		dot_product += h_dot_prod[i];
 		magnitude1 += h_mag1[i];
 		magnitude2 += h_mag2[i];
+		dot_product += h_dot_prod[i];
 	}
 
 	magnitude1 = sqrt(magnitude1);
 	magnitude2 = sqrt(magnitude2);
 
-	double result = acos(dot_product/(magnitude1*magnitude2));
+	//printf("magnitude1: %.2f, magnitude2: %.2f, dot_product: %.2f\n", (float)magnitude1, (float)magnitude2, (float)dot_product);
 
-	printf("Angle: %f\n", (180.0 / M_PI)*result);
+	double result = acos(dot_product/(magnitude1*magnitude2));
+	printf("\nResults\n____________________________________________________\n");
+	printf("CPU result: %f\n", cpu_result);
+	printf("GPU result: %f\n\n", (float)((180.0 / M_PI)*result));
 
 
 	cudaFree(d_A);
@@ -188,5 +289,6 @@ int main()
 	cudaFree(mag1);
 	cudaFree(mag2);
 
+	return 0;
 }
 
---
+++ src/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# run script
+
+/usr/local/cuda-9.1/bin/nvcc angle.cu -o angle
+
+rm -f gudukbay_gulsum.output
+touch gudukbay_gulsum.output
+
+n_values=(1000000 5000000 10000000)
+block_sizes=(32 64 128 256 512)
+
+
+for(( j = 0; j < 3; j++))
+do
+  echo -e "Number of Elements:${n_values[$j]}\n" >> gudukbay_gulsum.output
+  for (( i=0; i<5; i++ ))
+  do
+    echo -e "k=${block_sizes[$i]}\n" >> gudukbay_gulsum.output
+    ./angle ${n_values[$j]} ${block_sizes[$i]} >> gudukbay_gulsum.output
+    echo -e "\n" >> gudukbay_gulsum.output
+  done
+done
